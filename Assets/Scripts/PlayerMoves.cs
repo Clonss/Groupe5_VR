6c50eec7d13b5f485edf86ea79c5edd3b56a47c5
Lune <ilfautquejemensouvienne@gmail.com>
2019-04-18 09:57:53 +0200

    ceci est le déplacement qui TOUUUURNE

--- Assets/Scripts/PlayerMoves.cs
+++ Assets/Scripts/PlayerMoves.cs
@@ -1,27 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMoves : MonoBehaviour
 {
 
     public Transform pointerObj;
+    private NavMeshAgent myNMA;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myNMA = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));    // variable qui reçoit la position de la souris.
-        RaycastHit hit;        // variable qui permet de détecter les collision
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, 5f)) // On lance un rayon qui part de la camera, qui prend la direction du pointeur de la souris et qui a pour longueur 100.
+        if (Physics.Raycast(ray, out hit, 5f))
         {
-            pointerObj.position = hit.point;      // Alors on modifie la couleur de la dalle.
+            pointerObj.position = hit.point;
+            if (Input.GetMouseButtonDown(0))
+            {
+                myNMA.destination = hit.point;
+            }
         }
     }
 }

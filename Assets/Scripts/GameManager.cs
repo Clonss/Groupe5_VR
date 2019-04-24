71f81a3c9a4fa5517daed9b2b347c69ef7fabafd
Zetsubo9 <max.lebolloch@laposte.net>
2019-04-24 15:04:33 +0200

    Timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float duree;
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = duree;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer > 0)
+        {
+            timer-= 1* Time.deltaTime;
+        }
+        if(timer <= 0)
+        {
+            timer = 0;
+        }
     }
 }

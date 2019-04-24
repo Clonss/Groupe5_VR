1bb16acd8312361712fc8e05816d8accad9de4c7
Lune <ilfautquejemensouvienne@gmail.com>
2019-04-24 15:21:43 +0200

    essai

--- Assets/Scripts/PlayerMoves.cs
+++ Assets/Scripts/PlayerMoves.cs
@@ -3,23 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(SteamVR_TrackedController))]
 public class PlayerMoves : MonoBehaviour
 {
 
     public Transform pointerObj;
     private NavMeshAgent myNMA;
 
+    [SerializeField]
+
+    private SteamVR_TrackedController trackedController;
+
     // Start is called before the first frame update
     void Start()
     {
         myNMA = GetComponent<NavMeshAgent>();
+        trackedController = GetComponent<SteamVR_TrackedController>();
+        trackedController.PadClicked += Walk;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        
+    }
+
+    private void Walk(object sender, ClickedEventArgs e)
+    {
         RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+
 
         if (Physics.Raycast(ray, out hit, 5f))
         {
